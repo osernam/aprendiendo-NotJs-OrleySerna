9a98d68a8d778955871c8a33401c2232e27f64d7
osernam <osernam@gmail.com>
2022-08-04 20:57:05 -0500

    servidor con express

--- consultar2.js
+++ consultar2.js
@@ -2,14 +2,5 @@
 const express = require("express")
 app = exppress();
 
-app.get("/", function(req,res){
-    res.semd("hola"),
-    res.end();
-});
-
-//crear: post
-//read: get
-//uraid: put
-//delete: delete
 
 app.listen(7576);
--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-var server = require("./server")
+//var server = require("./server")
 
-server.iniciar(); // en server se exporto la funcion iniciar lo que nos permite iniciar el servidor donde se invoque la funcion
+//server.iniciar(); // en server se exporto la funcion iniciar lo que nos permite iniciar el servidor donde se invoque la funcion
+
+
+
+
+const express = require('express');
+const app= express();
+
+app.get("/", function(req,res){
+    res.semd("hola"),
+    res.end();
+});
+
+//crear: post
+//read: get
+//uraid: put
+//delete: delete
+
+app.listen(8520);
